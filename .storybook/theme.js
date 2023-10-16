bee3a8c3af17de0f1e4ddd6749020d0df4c18f5d
Matteo Antony Mistretta <m.mistretta@reply.it>
2023-10-16 22:35:53 +0200

    Provide a more fitting logo

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -4,6 +4,6 @@ export default create({
   base: 'dark',
   brandTitle: 'Inglorious Engine',
   brandUrl: '/',
-  brandImage: 'https://avatars.githubusercontent.com/u/13223783?s=200&v=4',
+  brandImage: '/logo.png',
   brandTarget: '_self',
 })
