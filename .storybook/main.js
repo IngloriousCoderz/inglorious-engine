89c55bc6c691d9bd7885774f2845d89fbb4015e6
Matteo Antony Mistretta <m.mistretta@reply.it>
2024-05-28 19:24:19 +0200

    Upgrade to latest Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,17 +5,24 @@ const config = {
     '../src/**/*.mdx',
     '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+    '@chromatic-com/storybook'
   ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 }
 export default config
