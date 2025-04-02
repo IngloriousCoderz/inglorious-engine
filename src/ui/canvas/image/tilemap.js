d6f68e1ff0353e5e5c6efbcc76aeb0ff5437987c
Matteo Antony Mistretta <m.mistretta@reply.it>
2025-04-02 11:36:59 +0200

    Add tilemap layers and bitmask flipping

--- src/ui/canvas/image/tilemap.js
+++ src/ui/canvas/image/tilemap.js
@@ -1,9 +1,22 @@
 import imageDraw from "./image.js"
 
+const DEFAULT_SCALE = 1
+
+const FLIP = -1
+const NO_FLIP = 1
+
+const CENTER_WIDTH = 2
+const CENTER_HEIGHT = 2
+
+const FLIPPED_HORIZONTALLY_FLAG = 0x80000000
+const FLIPPED_VERTICALLY_FLAG = 0x40000000
+// const FLIPPED_DIAGONALLY_FLAG = 0x20000000
+// const ROTATED_HEXAGONAL_120_FLAG = 0x10000000
+
 export default function draw(ctx, instance, options) {
   const { tilemap } = instance
-  const { image, tiles } = tilemap
-  const { imageSize, tileSize, columns, scale } = image
+  const { image, layers } = tilemap
+  const { imageSize, tileSize, columns, scale = DEFAULT_SCALE } = image
 
   const [imageWidth] = imageSize
   const [tileWidth, tileHeight] = tileSize
@@ -11,19 +24,37 @@ export default function draw(ctx, instance, options) {
 
   ctx.save()
 
-  tiles.forEach((tile, index) => {
-    const sx = tile % cols
-    const sy = Math.floor(tile / cols)
+  layers.forEach(({ tiles }) => {
+    tiles.forEach((flaggedTile, index) => {
+      const isFlippedHorizontally = !!(flaggedTile & FLIPPED_HORIZONTALLY_FLAG)
+      const isFlippedVertically = !!(flaggedTile & FLIPPED_VERTICALLY_FLAG)
 
-    const dx = (index % columns) * tileWidth
-    const dy = Math.floor(index / columns) * tileHeight
+      let tile = flaggedTile
+      tile &= ~FLIPPED_HORIZONTALLY_FLAG
+      tile &= ~FLIPPED_VERTICALLY_FLAG
 
-    ctx.save()
-    ctx.scale(scale, scale)
-    ctx.translate(dx, dy)
+      const sx = tile % cols
+      const sy = Math.floor(tile / cols)
 
-    imageDraw(ctx, { image, sx, sy }, options)
-    ctx.restore()
+      const dx = (index % columns) * tileWidth
+      const dy = Math.floor(index / columns) * tileHeight
+
+      ctx.save()
+
+      ctx.scale(scale, scale)
+      ctx.translate(dx, dy)
+
+      ctx.translate(tileWidth / CENTER_WIDTH, tileHeight / CENTER_HEIGHT)
+      ctx.scale(
+        isFlippedHorizontally ? FLIP : NO_FLIP,
+        isFlippedVertically ? FLIP : NO_FLIP,
+      )
+      ctx.translate(-tileWidth / CENTER_WIDTH, -tileHeight / CENTER_HEIGHT)
+
+      imageDraw(ctx, { image, sx, sy }, options)
+
+      ctx.restore()
+    })
   })
 
   ctx.restore()
