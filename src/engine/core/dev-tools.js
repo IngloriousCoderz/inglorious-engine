29e1586cd0e4d6d9c8efd4bd7c5a401d6eb4907d
Matteo Antony Mistretta <antony.mistretta@gmail.com>
2025-08-24 19:35:19 +0200

    Implement progammatic dispatching from Redux DevTools

--- src/engine/core/dev-tools.js
+++ src/engine/core/dev-tools.js
@@ -28,52 +28,14 @@ export function initDevTools(store) {
   })
 
   devToolsInstance.subscribe((message) => {
-    if (message.type !== "DISPATCH") {
-      return
-    }
-
-    switch (message.payload.type) {
-      // reset button
-      case "RESET": {
-        store.reset()
-        devToolsInstance.init(store.getState())
+    switch (message.type) {
+      case "DISPATCH":
+        handleDispatch(message, store)
         break
-      }
 
-      // revert button
-      case "ROLLBACK": {
-        const newState = JSON.parse(message.state)
-        store.setState(newState)
+      case "ACTION":
+        handleAction(message, store)
         break
-      }
-
-      // commit button
-      case "COMMIT": {
-        devToolsInstance.init(store.getState())
-        break
-      }
-
-      // import from file button
-      case "IMPORT_STATE": {
-        const { computedStates, actionsById } = message.payload.nextLiftedState
-
-        const [firstComputedState] = computedStates
-        const lastComputedState =
-          computedStates[computedStates.length - LAST_STATE]
-        if (lastComputedState) {
-          store.setState(lastComputedState.state)
-        }
-
-        const flattenedActions = Object.values(actionsById)
-          .flatMap(({ action }) => action.payload ?? action)
-          .map((action, index) => [index, action])
-
-        devToolsInstance.init(
-          firstComputedState.state,
-          Object.fromEntries(flattenedActions),
-        )
-        break
-      }
     }
   })
 
@@ -84,4 +46,55 @@ export function sendAction(action, state) {
   if (devToolsInstance) {
     devToolsInstance.send(action, state)
   }
+}
+
+function handleDispatch(message, store) {
+  switch (message.payload.type) {
+    // reset button
+    case "RESET": {
+      store.reset()
+      devToolsInstance.init(store.getState())
+      break
+    }
+
+    // revert button
+    case "ROLLBACK": {
+      const newState = JSON.parse(message.state)
+      store.setState(newState)
+      break
+    }
+
+    // commit button
+    case "COMMIT": {
+      devToolsInstance.init(store.getState())
+      break
+    }
+
+    // import from file button
+    case "IMPORT_STATE": {
+      const { computedStates, actionsById } = message.payload.nextLiftedState
+
+      const [firstComputedState] = computedStates
+      const lastComputedState =
+        computedStates[computedStates.length - LAST_STATE]
+      if (lastComputedState) {
+        store.setState(lastComputedState.state)
+      }
+
+      const flattenedActions = Object.values(actionsById)
+        .flatMap(({ action }) => action.payload ?? action)
+        .map((action, index) => [index, action])
+
+      devToolsInstance.init(
+        firstComputedState.state,
+        Object.fromEntries(flattenedActions),
+      )
+      break
+    }
+  }
+}
+
+function handleAction(message, store) {
+  const action = JSON.parse(message.payload)
+  store.dispatch(action)
 }
