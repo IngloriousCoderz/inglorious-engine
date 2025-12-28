2c0b1cb13f45efc032ed22eaf1efbe3295b7b9b6
Matteo Antony Mistretta <antony.mistretta@gmail.com>
2025-12-28 14:23:50 +0100

    Don't depend on Node for Vite config

--- packages/create-app/templates/js/eslint.config.js
+++ packages/create-app/templates/js/eslint.config.js
@@ -1,5 +1,1 @@
-import browserConfig from "@inglorious/eslint-config/browser"
-import nodeConfig from "@inglorious/eslint-config/node"
-import { defineConfig } from "eslint/config"
-
-export default defineConfig([...browserConfig, ...nodeConfig])
+export { default } from "@inglorious/eslint-config/browser"
--- packages/create-app/templates/js/vite.config.js
+++ packages/create-app/templates/js/vite.config.js
@@ -1,11 +1,16 @@
-import { resolve } from "path"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+
 import { defineConfig } from "vite"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 })
